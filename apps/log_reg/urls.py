70f1249589ca73defd9cee1b61ef55eb5b55d7c7
Kevin <stradtkt22@gmail.com>
2018-07-11 17:03:05 -0400

    Added changes to views.py and the dashboard page

--- apps/log_reg/urls.py
+++ apps/log_reg/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^login-page$', views.login_page),
     url(r'^register-page$', views.register_page),
+    url(r'^dashboard$', views.dashboard),
     url(r'^login$', views.login),
     url(r'^register$', views.register),
 ]
--- apps/log_reg/views.py
+++ apps/log_reg/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
+import bcrypt
 from .models import *
 # Create your views here.
 def index(request):
@@ -14,28 +15,32 @@ def login_page(request):
 def register_page(request):
     return render(request, 'log_reg/register.html')
 
+def dashboard(request):
+    context = {"user": User.objects.get(id=request.session['id'])}
+    return render(request, 'log_reg/dashboard.html', context)
+
 def register(request):
     first_name = request.POST['first_name']
     last_name = request.POST['last_name']
     email = request.POST['email']
     password = request.POST['password']
-    errors = User.objects.validate(first_name, last_name, email, password)
-    if len(errors) == 0:
-        User.objects.create(first_name=first_name, last_name=last_name, email=email, password=password)
-        return HttpResponse("Added to the database")
-    else:
-        for message in errors:
-            messages.error(request, message)
-    return redirect('/')
+    hashed_pw = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    user = User.objects.create(first_name=first_name, last_name=last_name, email=email, password=hashed_pw)
+    return redirect('/login-page')
 
 def login(request):
     email = request.POST['email']
     password = request.POST['password']
-    result = User.objects.validateLogin(email, password)
-    if result[0]:
-        request.session["id"] = result[1]
-        return redirect("/")
+    user = User.objects.filter(email=email)
+    if len(user) > 0:
+        is_pass = bcrypt.checkpw(password.encode(), user[0].password.encode())
+        if is_pass:
+            request.session['id'] = user[0].id
+            messages.success(request, 'Successfully Logged In')
+            return redirect('/dashboard')
+        else:
+            messages.error(request, "Incorrect items to login")
+            return redirect('/')
     else:
-        for message in result[1]:
-            messages.error(request, message)
-        return redirect("/")
+        messages.error(request, "User does not exists")
+        return redirect('/')
